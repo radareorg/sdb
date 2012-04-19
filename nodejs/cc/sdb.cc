62459a22cd381ce4984e7a6f678a5dbb989b7ae7
pancake <pancake@nopcode.org>
2012-04-19 02:28:49 +0200

    * More fixes and enhacements for the node-native module

--- nodejs/cc/sdb.cc
+++ nodejs/cc/sdb.cc
@@ -1,4 +1,5 @@
 /* node-sdb native module */
+/* author: pancake */
 
 #include <string>
 #include <node.h>
@@ -10,9 +11,8 @@ using namespace v8;
 
 class NodeSdb : public ObjectWrap {
 	public:
+#define getSdb() Unwrap<NodeSdb>(args.This())->sdb
 		Sdb *sdb;
-		static Persistent<FunctionTemplate> constructor_template;
-		static Persistent<FunctionTemplate> s_ct;
 
 		NodeSdb(const char *file, int lock) {
 			sdb = sdb_new (file, lock);
@@ -20,15 +20,22 @@ class NodeSdb : public ObjectWrap {
 
 		static Handle<Value> node_sdb_sync(const Arguments& args) {
 			HandleScope scope;
-			NodeSdb *ns = Unwrap<NodeSdb>(args.This());
-			int foo = sdb_sync(ns->sdb);
+			return scope.Close (sdb_sync (getSdb ())? True (): False ());
+		}
+
+		static Handle<Value> node_sdb_inc(const Arguments& args) {
+			HandleScope scope;
+			String::Utf8Value pa (args[0]->ToString ());
+			Local<Integer> pb (args[1]->ToInteger ());
+			// TODO: use pb!
+			int foo = sdb_inc (getSdb(), *pa, 1, 0);
 			return scope.Close (foo? True (): False ());
 		}
+
 		static Handle<Value> node_sdb_exists(const Arguments& args) {
 			HandleScope scope;
 			Local<String> a = args[0]->ToString (); String::Utf8Value pa(a);
-			NodeSdb *ns = Unwrap<NodeSdb>(args.This());
-			int foo = sdb_exists (ns->sdb, *pa);
+			int foo = sdb_exists (getSdb(), *pa);
 			return scope.Close (foo? True (): False ());
 		}
 
@@ -49,6 +56,10 @@ class NodeSdb : public ObjectWrap {
 			NODE_SET_METHOD (obj, "get", node_sdb_get);
 			NODE_SET_METHOD (obj, "jsonSet", node_sdb_json_set);
 			NODE_SET_METHOD (obj, "jsonGet", node_sdb_json_get);
+			//
+			NODE_SET_METHOD (obj, "inc", node_sdb_inc);
+		//	NODE_SET_METHOD (obj, "dec", node_sdb_dec);
+		//	NODE_SET_METHOD (obj, "add", node_sdb_add);
 			return scope.Close (obj);
 		}
 
@@ -56,45 +67,39 @@ class NodeSdb : public ObjectWrap {
 			return node_sdb_new (args);
 		}
 
-
 		static Handle<Value> node_sdb_json_get(const Arguments& args) {
 			HandleScope scope;
+			
 			Local<String> a = args[0]->ToString (); String::Utf8Value pa(a);
 			Local<String> b = args[1]->ToString (); String::Utf8Value pb(b);
-			NodeSdb *ns = Unwrap<NodeSdb>(args.This());
-			char *s = sdb_json_get (ns->sdb, *pa, *pb, NULL);
+			char *s = sdb_json_get (getSdb(), *pa, *pb, NULL);
 			if (s) return scope.Close (String::New (s));
 			return scope.Close (Null ());
 		}
 
 		static Handle<Value> node_sdb_json_set(const Arguments& args) {
 			HandleScope scope;
-			Local<String> a = args[0]->ToString (); String::Utf8Value pa(a);
-			Local<String> b = args[1]->ToString (); String::Utf8Value pb(b);
-			Local<String> c = args[2]->ToString (); String::Utf8Value pc(c);
+			String::Utf8Value pa (args[0]->ToString ());
+			String::Utf8Value pb (args[1]->ToString ());
+			String::Utf8Value pc (args[2]->ToString ());
 			// CAS Local<String> c = args[2]->ToInteger(); 
-			NodeSdb *ns = Unwrap<NodeSdb>(args.This());
-			int ret = sdb_json_set (ns->sdb, *pa, *pb, *pc, NULL);
+			int ret = sdb_json_set (getSdb (), *pa, *pb, *pc, NULL);
 			if (ret) return scope.Close (Integer::New (ret));
 			return scope.Close (Null ());
 		}
 
 		static Handle<Value> node_sdb_get(const Arguments& args) {
 			HandleScope scope;
-			Local<String> a = args[0]->ToString ();
-			String::Utf8Value pa(a);
-			NodeSdb *ns = Unwrap<NodeSdb>(args.This());
-			char *s = sdb_get (ns->sdb, *pa, NULL);
+			String::Utf8Value pa (args[0]->ToString ());
+			char *s = sdb_get (getSdb (), *pa, NULL);
 			if (s) return scope.Close (String::New (s));
 			return scope.Close (Null ());
 		}
 
 		static Handle<Value> node_sdb_set(const Arguments& args) {
 			HandleScope scope;
-			Local<String> a = args[0]->ToString ();
-			Local<String> b = args[1]->ToString ();
-			String::Utf8Value pa(a);
-			String::Utf8Value pb(b);
+			String::Utf8Value pa (args[0]->ToString ());
+			String::Utf8Value pb (args[1]->ToString ());
 			NodeSdb *s = ObjectWrap::Unwrap<NodeSdb>(args.This());
 			int ret = -1;
 			if (s && s->sdb)
@@ -116,7 +121,6 @@ class NodeSdb : public ObjectWrap {
 
 extern "C" {
 	void init(Handle<Object> target) {
-		printf ("cinit\n");
 		NodeSdb::Initialize(target);
 	}
 
--- nodejs/cc/test.js
+++ nodejs/cc/test.js
@@ -1,25 +1,29 @@
 /* node sdb test */
 
-var sdb = require ("./build/Release/sdb");
+var sdb = require ("./sdb");
 var util = require ("util");
+var p = console.log;
 
 console.log (sdb);
 var db = new sdb.open ("db.sdb");
 //console.log (db);
-console.log ("version: "+sdb.version);
-console.log ("file: "+db.file);
-	db.set ("g", 
+p ("version: "+sdb.version);
+p ("file: "+db.file);
+db.set ("g", 
 "{\"glossary\":{\"title\":\"example glossary\",\"page\":1,\"GlossDiv\":{\"title\":\"First gloss title\",\"GlossList\":{\"GlossEntry\":{\"ID\":\"SGML\",\"SortAs\":\"SGML\",\"GlossTerm\":\"Standard Generalized Markup Language\",\"Acronym\":\"SGML\",\"Abbrev\":\"ISO 8879:1986\",\"GlossDef\":{\"para\":\"A meta-markup language, used to create markup languages such as DocBook.\",\"GlossSeeAlso\":[\"OK\",\"GML\",\"XML\"]},\"GlossSee\":\"markup\"}}}}} "
 );
 
-var lala = db.jsonSet ("g", "glossary.title", "enis");
+// XXX: quotes are not properly supported
+var lala = db.jsonSet ("g", "glossary.title", "this is\" my message");
 var lala = db.jsonGet ("g", "glossary.title");
-console.log (lala);
+p (lala);
 var r = db.set ("foo", "bar");
-console.log (db.get ("foo"));
-console.log (db.get ("cow"));
+p (db.get ("foo"));
+p (db.get ("cow"));
 
-console.log (db.exists ("foo"));
-console.log (db.exists ("cow"));
+p (db.exists ("foo"));
+p (db.exists ("cow"));
+
+db.sync ();
 
 // var s = new Sdb (null);
