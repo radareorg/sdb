2775caa6711321cd945fbaf150a225e6684b13fc
pancake <pancake@nopcode.org>
2012-09-13 02:11:12 +0200

    Add some 'D' bindings, move langs to benchmark/

---
+++ bindings/d/sdb.d
@@ -0,0 +1,57 @@
+import std.stdio;
+import std.stdio;
+import std.conv;
+import std.string;
+import core.stdc.string;
+
+extern(C) {
+	void* sdb_new (const char *dir, int lock);
+	char* sdb_get (void*, const char *key, uint* cas);
+	int sdb_delete (void*, const char *key, uint cas);
+	int sdb_set (void*, const char *key, const char *data, uint cas);
+	void sdb_free (void* s);
+}
+
+class Sdb {
+	private void *db;
+	~this() {
+		sdb_free (db);
+	}
+
+	this() {
+		db = sdb_new (null, 0);
+		writeln ("Hello World");
+	}
+
+	this (const char *file) {
+		db = sdb_new (file, 0);
+		writeln ("Hello World on file ", file);
+	}
+
+	public bool remove(string key, uint cas=0) {
+		return sdb_delete (db, cast(char*)key, cas) != 0;
+	}
+
+	public bool set(string key, string value, uint cas=0) {
+		return sdb_set (db, cast(char*)key, cast(char*)value, cas) != 0;
+	}
+
+	public string get(string key, uint* cas=null) {
+		char* s = sdb_get (db, cast(char*)key, cas);
+		return s? to!string (s): null;
+	}
+}
+
+int main() {
+	auto s = new Sdb ("jaja");
+
+	s.set ("Hello", "hello");
+	writeln ("output: ", s.get ("Hello"));
+	s.set ("Hello", "world");
+	writeln ("output: ", s.get ("Hello"));
+	s.set ("Hello", "world");
+	writeln ("output: ", s.get ("Hello"));
+	s.remove ("Hello");
+	writeln ("output: ", s.get ("Hello"));
+	return 0;
+}
