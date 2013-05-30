c8a8250d0d9808e2b70a63cc3002f04eaff8ebf8
pancake <pancake@nopcode.org>
2013-05-31 01:00:01 +0200

    Add bench.c

---
+++ test/bench.c
@@ -0,0 +1,12 @@
+#include <sdb.h>
+main(int argc, char **argv) {
+	Sdb *s = sdb_new (NULL, 0);
+	int len = atoi (argv[1]);
+	int i = 0; 
+	char k[32];
+	for(i=0;i<len; i++)  {
+		sprintf (k, "keynum%d", i);
+		sdb_set (s, k, "jkladjfklafjklsajfklsajdflksadjfklsadjflk", 0);
+	}
+	sdb_free (s);
+}
