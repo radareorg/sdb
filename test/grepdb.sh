cb5368945e439d92121f07d698ecc42c8dacc93f
pancake <pancake@nopcode.org>
2013-12-20 01:49:58 +0100

    Add grepdb.sh toy

---
+++ test/grepdb.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+# toy implementation of key-value in shellscript
+db() {
+	ARG="$1"
+	if [ -z "$ARG" ]; then
+		[ $n != 0 ] && return 1
+		cat "$FILE"
+	elif [ -z "`echo \"$ARG\" | grep =`" ]; then
+		grep "^${ARG}=" "$FILE" | cut -d = -f 2-
+		# grep = $FILE  # is grep faster than cat??
+	else
+		KEY="`echo $ARG|cut -d = -f 1`"
+		VAL="`echo $ARG| cut -d = -f 2`"
+		if [ -z "$VAL" ]; then
+			grep -v "^$KEY=" "$FILE" > ".$FILE"
+			mv ".$FILE" "$FILE"
+		else
+			grep -v "^$KEY=" "$FILE" > ".$FILE"
+			echo "$ARG" >> ".$FILE"
+			mv ".$FILE" "$FILE"
+		fi
+	fi
+	return 0
+}
+
+if [ -z "$1" ]; then
+	echo "Usage: sdb.sh file [key[=value]]"
+	exit 1
+fi
+FILE="$1"
+if [ "$FILE" = - ]; then
+	FILE=_
+	if [ -z "$2" ]; then
+		while : ; do
+			read A
+			db "$A"
+		done
+	fi
+fi
+touch "$FILE"
+
+n=0
+while : ; do
+	ARG="$2"
+	db "$2"
+	[ $? = 1 ] && break
+	shift
+	n=$(($N+1))
+done
+[ "$FILE" = - ] && rm -f "$FILE"
