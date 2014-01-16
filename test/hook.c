1e2dfe6d5a959a90e6a9682aea08f1637adb1d11
pancake <pancake@nopcode.org>
2014-01-16 01:10:01 +0100

    Fix main in hook example

--- test/hook.c
+++ test/hook.c
@@ -4,9 +4,10 @@ void ptr(void *user, const char *k, const char *v) {
 	printf ("%s %s\n", k, v);
 }
 
-main() {
+int main() {
 	Sdb *s = sdb_new (NULL, 0);
 	sdb_hook (s, ptr, NULL);
 	sdb_set (s, "Hello", "World", 0);
 	sdb_free (s);
+	return 0;
 }
