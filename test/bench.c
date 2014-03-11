5cf6d324d790ea094a836a3c0c91c8ac6399b3ea
pancake <pancake@nopcode.org>
2014-03-11 01:16:04 +0100

    Add one/ target to create standalone .h and .c file

--- test/bench.c
+++ test/bench.c
@@ -1,12 +1,13 @@
 #include <sdb.h>
 main(int argc, char **argv) {
-	Sdb *s = sdb_new (NULL, 0);
+	Sdb *s = sdb_new (NULL, "____", 0);
 	int len = atoi (argv[1]);
 	int i = 0; 
 	char k[32];
-	for(i=0;i<len; i++)  {
+	for (i=0;i<len; i++) {
 		sprintf (k, "keynum%d", i);
 		sdb_set (s, k, "jkladjfklafjklsajfklsajdflksadjfklsadjflk", 0);
 	}
+	sdb_sync (s);
 	sdb_free (s);
 }
