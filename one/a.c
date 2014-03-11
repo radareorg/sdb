ae4aaa29ccfeb948caec7b96064b4b5b7ad9a133
pancake <pancake@nopcode.org>
2014-03-11 02:40:36 +0100

    Annotate benchmark

--- one/a.c
+++ one/a.c
@@ -3,7 +3,7 @@
 int main() {
 	printf ("Testing foo\n");
 	Sdb *s = sdb_new (NULL, NULL, 0);
-	//sdb_set (s, "Hello", "World", 0);
+	sdb_set (s, "Hello", "World", 0);
 sdb_array_push (s, "Hello", ":D",0);
 	printf ("Hello %s\n", sdb_const_get (s, "Hello", 0));
 	sdb_free (s);
--- test/bench.c
+++ test/bench.c
@@ -1,13 +1,19 @@
 #include <sdb.h>
-main(int argc, char **argv) {
+
+int main(int argc, char **argv) {
 	Sdb *s = sdb_new (NULL, "____", 0);
 	int len = atoi (argv[1]);
 	int i = 0; 
 	char k[32];
+// 1s
 	for (i=0;i<len; i++) {
 		sprintf (k, "keynum%d", i);
 		sdb_set (s, k, "jkladjfklafjklsajfklsajdflksadjfklsadjflk", 0);
 	}
+// 800 ms - if no db
+// 2.8 s  - if db exist
 	sdb_sync (s);
+// 200 ms
 	sdb_free (s);
+	return 0;
 }
