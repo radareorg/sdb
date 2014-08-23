2a5f9399194e18e8f4d23073c034e14363bd04fd
pancake <pancake@nopcode.org>
2014-08-23 10:42:10 +0200

    Fix compilation warnings in test/stack.c

--- test/stack.c
+++ test/stack.c
@@ -1,17 +1,19 @@
 #include <sdb.h>
 
 int main() {
-	const char *p;
+	const char *cp;
+	char *p;
+
 	Sdb *s = sdb_new (NULL, NULL, 0);
 	sdb_set (s, "array", "111,222,333", 0);
 	sdb_array_delete (s, "array", 1, 0);
-	p = sdb_const_get (s, "array", 0);
-	printf ("--> %s\n", p);
+	cp = sdb_const_get (s, "array", 0);
+	printf ("--> %s\n", cp);
 	printf ("--> %d\n", sdb_array_length (s, "array"));
 	sdb_array_push (s, "array", "end", 0);
 	sdb_array_push (s, "array", "end2", 0);
-	p = sdb_const_get (s, "array", 0);
-	printf ("--> %s\n", p);
+	cp = sdb_const_get (s, "array", 0);
+	printf ("--> %s\n", cp);
 	printf ("--> %d\n", sdb_array_length (s, "array"));
 	sdb_array_push (s, "array", "end2", 0);
 
