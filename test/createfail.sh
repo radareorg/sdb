70ab3900ce0646f884abcb7c0730d1bc36a3e7da
pancake <pancake@nopcode.org>
2015-05-12 14:51:12 +0200

    Add more createfail tests

--- test/createfail.sh
+++ test/createfail.sh
@@ -2,6 +2,9 @@
 export LANG=C
 export LC_ALL=C
 
+if [ "$1" = wine ]; then
+	SDB="wine ../src/sdb.exe"
+fi
 if [ -z "${SDB}" ]; then
 #	if [ -f ../src/sdb.exe ]; then
 #		SDB="wine ../src/sdb.exe"
@@ -9,7 +12,12 @@ if [ -z "${SDB}" ]; then
 		SDB="../src/sdb"
 #	fi
 fi
+WINEMODE=0
+echo "$SDB" |grep -q wine && WINEMODE=1
 echo "[+] Using SDB: ${SDB}"
+
+##########################
+
 N="\033[0m"
 R="\033[31m"
 G="\033[32m"
@@ -19,8 +27,6 @@ ${SDB} .newdb a=c
 ${SDB} .newdb
 
 RET=0
-WINEMODE=0
-echo "$SDB" |grep -q wine && WINEMODE=1
 if [ "${WINEMODE}" = 1 ]; then
 	A="`${SDB} .newdb | perl -0 -pe 's/\r//g;s/\n\Z//;'`"
 else
@@ -45,4 +51,60 @@ else
 	printf "$R  Recreate test   FAIL$N\n"
 	RET=1
 fi
+
+rm -f .newdb
+echo a=c | ${SDB} .newdb =
+if [ "${WINEMODE}" = 1 ]; then
+	A="`${SDB} .newdb | perl -0 -pe 's/\r//g;s/\n\Z//;'`"
+else
+	A="`${SDB} .newdb`"
+fi
+if [ "a=c" = "$A" ]; then
+	printf "$G  Make test       OK$N\n"
+else
+	printf "$R  Make test       FAIL$N\n"
+	RET=1
+fi
+
+
+echo a=d | ${SDB} .newdb =
+if [ "${WINEMODE}" = 1 ]; then
+	A="`${SDB} .newdb | perl -0 -pe 's/\r//g;s/\n\Z//;'`"
+else
+	A="`${SDB} .newdb`"
+fi
+if [ "a=d" = "$A" ]; then
+	printf "$G  Remake test     OK$N\n"
+else
+	printf "$R  Remake test     FAIL$N\n"
+	RET=1
+fi
+
+rm -f .newdb
+echo a=c | ${SDB} .newdb -
+if [ "${WINEMODE}" = 1 ]; then
+	A="`${SDB} .newdb | perl -0 -pe 's/\r//g;s/\n\Z//;'`"
+else
+	A="`${SDB} .newdb`"
+fi
+if [ "a=c" = "$A" ]; then
+	printf "$G  Dash test       OK$N\n"
+else
+	printf "$R  Dash test       FAIL$N\n"
+	RET=1
+fi
+
+echo a=d | ${SDB} .newdb -
+if [ "${WINEMODE}" = 1 ]; then
+	A="`${SDB} .newdb | perl -0 -pe 's/\r//g;s/\n\Z//;'`"
+else
+	A="`${SDB} .newdb`"
+fi
+if [ "a=d" = "$A" ]; then
+	printf "$G  Redash test     OK$N\n"
+else
+	printf "$R  Redask test     FAIL$N\n"
+	RET=1
+fi
+
 exit $RET
