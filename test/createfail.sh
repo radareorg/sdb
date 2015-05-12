2abf257d995b2a57d8794170c3730b5554300f31
pancake <pancake@nopcode.org>
2015-05-12 03:07:55 +0200

    More and better tests for create and grep

--- test/createfail.sh
+++ test/createfail.sh
@@ -1,28 +1,48 @@
 #!/bin/sh
+export LANG=C
+export LC_ALL=C
+
 if [ -z "${SDB}" ]; then
-	if [ -f ../src/sdb.exe ]; then
-		SDB="wine ../src/sdb.exe"
-	else
+#	if [ -f ../src/sdb.exe ]; then
+#		SDB="wine ../src/sdb.exe"
+#	else
 		SDB="../src/sdb"
-	fi
+#	fi
 fi
+echo "[+] Using SDB: ${SDB}"
 N="\033[0m"
 R="\033[31m"
 G="\033[32m"
 echo "[+] Running createfail.sh..."
 rm -f .newdb
 ${SDB} .newdb a=c
-if [ "a=c" = "`sdb .newdb`" ]; then
+${SDB} .newdb
+
+RET=0
+WINEMODE=0
+echo "$SDB" |grep -q wine && WINEMODE=1
+if [ "${WINEMODE}" = 1 ]; then
+	A="`${SDB} .newdb | perl -0 -pe 's/\r//g;s/\n\Z//;'`"
+else
+	A="`${SDB} .newdb`"
+fi
+if [ "a=c" = "$A" ]; then
 	printf "$G  Create test     OK$N\n"
 else
 	printf "$R  Create test     FAIL$N\n"
-	exit 1
+	RET=1
 fi
+
 ${SDB} .newdb a=d
-if [ "a=d" = "`sdb .newdb`" ]; then
+if [ "${WINEMODE}" = 1 ]; then
+	A="`${SDB} .newdb | perl -0 -pe 's/\r//g;s/\n\Z//;'`"
+else
+	A="`${SDB} .newdb`"
+fi
+if [ "a=d" = "$A" ]; then
 	printf "$G  Recreate test   OK$N\n"
 else
 	printf "$R  Recreate test   FAIL$N\n"
-	exit 1
+	RET=1
 fi
-exit 0
+exit $RET
---
+++ test/testgrep.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+# proof that grep is faster than sdb
+
+printf "\033[33m[+] Creating test database...  \033[0m"
+sh add10k.sh > /dev/null 2>&1
+echo "DONE"
+
+# creates test.db and 'a'
+G=6561
+T=a
+D=test.db
+K=key29988
+
+echo
+printf "\033[33m[+] System grep    \033[0m"
+echo "$ grep 6561 $T"
+LINES=`time grep $G $T |wc -l`
+echo "\033[32mLines: ${LINES}\033[0m"
+
+echo
+printf "\033[33m[+] Dump and grep  \033[0m"
+echo "$ sdb $D | grep $G"
+LINES=`time sdb $D | grep $G | wc -l`
+echo "\033[32mLines: ${LINES}\033[0m"
+
+echo
+printf "\033[33m[+] Internal grep  \033[0m"
+echo "$ sdb -g $G $D"
+LINES=`time sdb -g $G $D | wc -l`
+echo "\033[32mLines: ${LINES}\033[0m"
+
+
+#########################
+echo
+printf "\033[33m[+] Grep get \033[0m"
+echo '$ grep "^$K=" $T'
+LINES="`time grep "^$K=" $T |wc -l `"
+echo "\033[32mLines: ${LINES}\033[0m"
+
+
+echo
+printf "\033[33m[+] Sdb get \033[0m"
+echo "$ sdb $D $K"
+LINES=`time sdb $D $K | grep -v '^$' | wc -l`
+echo "\033[32mLines: ${LINES}\033[0m"
+
