cb34ea0c1910f8b4b8d3f6cce0d53f7f6aa12fb6
pancake <pancake@nopcode.org>
2016-10-24 22:46:17 +0200

    Add two more sdb_delete tests

--- test/unit/test_sdb.c
+++ test/unit/test_sdb.c
@@ -40,9 +40,43 @@ bool test_sdb_list_delete(void) {
 	mu_end;
 }
 
+bool test_sdb_delete_none(void) {
+	Sdb *db = sdb_new (NULL, NULL, false);
+	sdb_set (db, "foo", "bar", 0);
+	sdb_set (db, "bar", "cow", 0);
+	sdb_set (db, "low", "bar", 0);
+	sdb_unset (db, "fundas", 0);
+	sdb_unset (db, "barnas", 0);
+	sdb_unset (db, "bar", 0);
+	sdb_unset (db, "pinuts", 0);
+	SdbList *list = sdb_foreach_list (db);
+	mu_assert_eq ((int)ls_length (sdb_foreach_list (db)), 2, "Unmatched rows");
+	sdb_free (db);
+	mu_end;
+}
+
+bool test_sdb_delete_alot(void) {
+	char key[128];
+	Sdb *db = sdb_new (NULL, NULL, false);
+	const int count = 2048;
+	int i;
+	for (i = 0; i < count; i++) {
+		sdb_set (db, sdb_fmt (0, "key.%d", i), "bar", 0);
+	}
+	for (i = 0; i < count; i++) {
+		sdb_unset (db, sdb_fmt (0, "key.%d", i), 0);
+	}
+	SdbList *list = sdb_foreach_list (db);
+	mu_assert_eq ((int)ls_length (sdb_foreach_list (db)), 0, "Unmatched rows");
+	sdb_free (db);
+	mu_end;
+}
+
 int all_tests() {
 	mu_run_test (test_sdb_foreach_delete);
 	mu_run_test (test_sdb_list_delete);
+	mu_run_test (test_sdb_delete_none);
+	mu_run_test (test_sdb_delete_alot);
 	return tests_passed != tests_run;
 }
 
