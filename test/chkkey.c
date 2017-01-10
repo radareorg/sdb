bb43968ca648c0712e30f775cd41f9236ff176c0
pancake <pancake@nopcode.org>
2017-01-10 12:11:08 +0100

    Deprecate the chkkey test

--- test/chkkey.c
+++ test/chkkey.c
@@ -4,10 +4,13 @@
 
 int main() {
 	int i;
+	eprintf ("sdb_check_key: has been deprecated\n");
+#if 0
 	for (i = 0; i < MAX ; i++) {
 		if (!sdb_check_key ("foo123bar.cow0x830803")) {
 			return 1;
 		}
 	}
+#endif
 	return 0;
 }
