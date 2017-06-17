9b31c6cf19d041ebd7cb671adeb1f4f92bc635ab
xarkes <xarkes@users.noreply.github.com>
2017-06-18 01:08:39 +0200

    Fix crash on windows (#141)

--- src/sdb.c
+++ src/sdb.c
@@ -551,7 +551,11 @@ static ut32 sdb_set_internal(Sdb* s, const char *key, char *val, int owned, ut32
 		return 0;
 	}
 	if (!val) {
-		val = "";
+		if (owned) {
+			val = strdup ("");
+		} else {
+			val = "";
+		}
 	}
 	// XXX strlen computed twice.. because of check_*()
 	klen = strlen (key);
