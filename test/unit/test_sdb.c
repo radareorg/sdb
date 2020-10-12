ee24c44312a5130fc5e811daa5bf53934cafe922
Florian Märkl <info@florianmaerkl.de>
2020-10-12 18:41:37 +0200

    Add plaintext SDB format (#219)

--- test/unit/test_sdb.c
+++ test/unit/test_sdb.c
@@ -193,17 +193,290 @@ bool test_sdb_copy() {
 	sdb_free (src);
 
 	mu_assert_eq (sdb_count (dst), 2, "root count");
-	mu_assert_streq (sdb_get (dst, "i am", 0), "thou", "root entries");
-	mu_assert_streq (sdb_get (dst, "thou art", 0), "i", "root entries");
+	mu_assert_streq (sdb_const_get (dst, "i am", 0), "thou", "root entries");
+	mu_assert_streq (sdb_const_get (dst, "thou art", 0), "i", "root entries");
 	mu_assert_eq (ls_length (dst->ns), 1, "sub ns count");
 	Sdb *dst_sub = sdb_ns (dst, "subns", false);
 	mu_assert_notnull (dst_sub, "subns");
 	mu_assert_eq (sdb_count (dst_sub), 3, "sub ns entries count");
-	mu_assert_streq (sdb_get (dst_sub, "radare", 0), "cool", "sub ns entries");
-	mu_assert_streq (sdb_get (dst_sub, "radare2", 0), "cooler", "sub ns entries");
-	mu_assert_streq (sdb_get (dst_sub, "cutter", 0), "coolest", "sub ns entries");
+	mu_assert_streq (sdb_const_get (dst_sub, "radare", 0), "cool", "sub ns entries");
+	mu_assert_streq (sdb_const_get (dst_sub, "radare2", 0), "cooler", "sub ns entries");
+	mu_assert_streq (sdb_const_get (dst_sub, "cutter", 0), "coolest", "sub ns entries");
 
 	sdb_free (dst);
+	mu_end;
+}
+
+#define PERTURBATOR "\\,\";]\n [\r}{'=/"
+
+static const char *text_ref_simple =
+	"/\n"
+	"aaa=stuff\n"
+	"bbb=other stuff\n"
+	"somekey=somevalue\n"
+	"\n"
+	"/subnamespace\n"
+	"\\/more stuff=in sub\n"
+	"key in sub=value in sub\n"
+	"\n"
+	"/subnamespace/subsub\n"
+	"some stuff=also down here\n";
+
+// the order in here is implementation-defined
+static const char *text_ref_simple_unsorted =
+	"/\n"
+	"aaa=stuff\n"
+	"somekey=somevalue\n"
+	"bbb=other stuff\n"
+	"\n"
+	"/subnamespace\n"
+	"key in sub=value in sub\n"
+	"\\/more stuff=in sub\n"
+	"\n"
+	"/subnamespace/subsub\n"
+	"some stuff=also down here\n";
+
+static const char *text_ref =
+	"/\n"
+	"\\\\,\";]\\n [\\r}{'\\=/key\\\\,\";]\\n [\\r}{'\\=/=\\\\,\";]\\n [\\r}{'=/value\\\\,\";]\\n [\\r}{'=/\n"
+	"aaa=stuff\n"
+	"bbb=other stuff\n"
+	"\n"
+	"/sub\\\\,\";]\\n [\\r}{'=\\/namespace\n"
+	"\\/more stuff\\n=\\nin\\nsub\\n\n"
+	"key\\\\,\";]\\n [\\r}{'\\=/in sub=value\\\\,\";]\\n [\\r}{'=/in sub\n"
+	"\n"
+	"/sub\\\\,\";]\\n [\\r}{'=\\/namespace/subsub\n"
+	"some stuff=also down here\n";
+
+static const char *text_ref_bad_nl =
+	"/\r\n"
+	"non=unix\r"
+	"newlines=should\n"
+	"be=banned";
+
+static const char *text_ref_broken =
+	"/////\n"
+	"just garbage\n"
+	"no\\=equal\\=here\n"
+	"=nokey\n"
+	"novalue=\n"
+	"more/garbage/////\n"
+	"\\/\\/\\/unnecessary=\\/escapes\\u\\x\\a\n"
+	"////some/////subns/\n"
+	"more=equal=signs=than=one=\n"
+	"////some//subns//more\n"
+	"////some//subns//more/further//////\n"
+	"escape=newlines\\\n"
+	"also escape=nothingness\\";
+
+static Sdb *text_ref_simple_db() {
+	Sdb *db = sdb_new0 ();
+	sdb_set (db, "somekey", "somevalue", 0);
+	sdb_set (db, "aaa", "stuff", 0);
+	sdb_set (db, "bbb", "other stuff", 0);
+
+	Sdb *sub = sdb_ns (db, "subnamespace", true);
+	sdb_set (sub, "key in sub", "value in sub", 0);
+	sdb_set (sub, "/more stuff", "in sub", 0);
+
+	Sdb *subsub = sdb_ns (sub, "subsub", true);
+	sdb_set (subsub, "some stuff", "also down here", 0);
+	return db;
+}
+
+static Sdb *text_ref_db() {
+	Sdb *db = sdb_new0 ();
+	sdb_set (db, PERTURBATOR"key"PERTURBATOR, PERTURBATOR"value"PERTURBATOR, 0);
+	sdb_set (db, "aaa", "stuff", 0);
+	sdb_set (db, "bbb", "other stuff", 0);
+
+	Sdb *sub = sdb_ns (db, "sub"PERTURBATOR"namespace", true);
+	sdb_set (sub, "key"PERTURBATOR"in sub", "value"PERTURBATOR"in sub", 0);
+	sdb_set (sub, "/more stuff\n", "\nin\nsub\n", 0);
+
+	Sdb *subsub = sdb_ns (sub, "subsub", true);
+	sdb_set (subsub, "some stuff", "also down here", 0);
+	return db;
+}
+
+static Sdb *text_ref_bad_nl_db() {
+	Sdb *db = sdb_new0 ();
+	sdb_set (db, "non", "unix", 0);
+	sdb_set (db, "newlines", "should", 0);
+	sdb_set (db, "be", "banned", 0);
+	return db;
+}
+
+static Sdb *text_ref_broken_db() {
+	Sdb *db = sdb_new0 ();
+	sdb_set (db, "///unnecessary", "/escapesuxa", 0);
+	Sdb *a = sdb_ns (db, "some", true);
+	Sdb *b = sdb_ns (a, "subns", true);
+	sdb_set (b, "more", "equal=signs=than=one=", 0);
+	Sdb *c = sdb_ns (b, "more", true);
+	Sdb *d = sdb_ns (c, "further", true);
+	sdb_set (d, "escape", "newlines", 0);
+	sdb_set (d, "also escape", "nothingness", 0);
+	return db;
+}
+
+static int tmpfile_new (const char *filename, const char *buf, size_t sz) {
+	int fd = open (filename, O_RDWR | O_CREAT | O_TRUNC | O_BINARY, 0644);
+	if (fd < 0) {
+		return -1;
+	}
+	if (!buf) {
+		return fd;
+	}
+	int w = write (fd, buf, sz);
+	if (w < (int)sz) {
+		close (fd);
+		return -1;
+	}
+	lseek (fd, 0, SEEK_SET);
+	return fd;
+}
+
+#define TEST_BUF_SZ 0x1000
+
+bool test_sdb_text_save_simple() {
+	Sdb *db = text_ref_simple_db ();
+
+	int fd = tmpfile_new (".text_save_simple", NULL, 0);
+	bool succ = sdb_text_save_fd (db, fd, true);
+	lseek (fd, 0, SEEK_SET);
+	char buf[TEST_BUF_SZ] = { 0 };
+	read (fd, buf, sizeof (buf) - 1);
+	close (fd);
+	unlink (".text_save_simple");
+
+	sdb_free (db);
+
+	mu_assert_true (succ, "save success");
+	mu_assert_streq (buf, text_ref_simple, "text save");
+
+	mu_end;
+}
+
+bool test_sdb_text_save_simple_unsorted() {
+	Sdb *db = text_ref_simple_db ();
+
+	int fd = tmpfile_new (".text_save_simple_unsorted", NULL, 0);
+	bool succ = sdb_text_save_fd (db, fd, false);
+	lseek (fd, 0, SEEK_SET);
+	char buf[TEST_BUF_SZ] = { 0 };
+	read (fd, buf, sizeof (buf) - 1);
+	close (fd);
+	unlink (".text_save_simple_unsorted");
+
+	sdb_free (db);
+
+	mu_assert_true (succ, "save success");
+	mu_assert_streq (buf, text_ref_simple_unsorted, "text save");
+
+	mu_end;
+}
+
+bool test_sdb_text_save() {
+	Sdb *db = text_ref_db ();
+
+	int fd = tmpfile_new (".text_save", NULL, 0);
+	bool succ = sdb_text_save_fd (db, fd, true);
+	lseek (fd, 0, SEEK_SET);
+	char buf[TEST_BUF_SZ] = { 0 };
+	read (fd, buf, sizeof (buf) - 1);
+	close (fd);
+	unlink (".text_save");
+
+	sdb_free (db);
+
+	mu_assert_true (succ, "save success");
+	mu_assert_streq (buf, text_ref, "text save");
+
+	mu_end;
+}
+
+static void diff_cb(const SdbDiff *diff, void *user) {
+	char buf[2048];
+	if (sdb_diff_format (buf, sizeof (buf), diff) < 0) {
+		return;
+	}
+	printf ("%s\n", buf);
+}
+
+bool test_sdb_text_load_simple() {
+	char *buf = strdup (text_ref_simple);
+	Sdb *db = sdb_new0 ();
+	bool succ = sdb_text_load_buf (db, buf, strlen (buf));
+	free (buf);
+
+	mu_assert_true (succ, "load success");
+	Sdb *ref_db = text_ref_simple_db ();
+	bool eq = sdb_diff (ref_db, db, diff_cb, NULL);
+	sdb_free (ref_db);
+	sdb_free (db);
+	mu_assert_true (eq, "load correct");
+	mu_end;
+}
+
+bool test_sdb_text_load() {
+	char *buf = strdup (text_ref);
+	Sdb *db = sdb_new0 ();
+	bool succ = sdb_text_load_buf (db, buf, strlen (buf));
+	free (buf);
+
+	mu_assert_true (succ, "load success");
+	Sdb *ref_db = text_ref_db ();
+	bool eq = sdb_diff (ref_db, db, diff_cb, NULL);
+	sdb_free (ref_db);
+	sdb_free (db);
+	mu_assert_true (eq, "load correct");
+	mu_end;
+}
+
+bool test_sdb_text_load_bad_nl() {
+	char *buf = strdup (text_ref_bad_nl);
+	Sdb *db = sdb_new0 ();
+	bool succ = sdb_text_load_buf (db, buf, strlen (buf));
+	free (buf);
+
+	mu_assert_true (succ, "load success");
+	Sdb *ref_db = text_ref_bad_nl_db ();
+	bool eq = sdb_diff (ref_db, db, diff_cb, NULL);
+	sdb_free (ref_db);
+	sdb_free (db);
+	mu_assert_true (eq, "load correct");
+	mu_end;
+}
+
+bool test_sdb_text_load_broken() {
+	char *buf = strdup (text_ref_broken);
+	Sdb *db = sdb_new0 ();
+	bool succ = sdb_text_load_buf (db, buf, strlen (buf));
+	free (buf);
+
+	mu_assert_true (succ, "load success");
+	Sdb *ref_db = text_ref_broken_db ();
+	bool eq = sdb_diff (ref_db, db, diff_cb, NULL);
+	sdb_free (ref_db);
+	sdb_free (db);
+	mu_assert_true (eq, "load correct");
+	mu_end;
+}
+
+bool test_sdb_text_load_file() {
+	close (tmpfile_new (".text_load_simple", text_ref_simple, strlen (text_ref_simple)));
+	Sdb *db = sdb_new0 ();
+	bool succ = sdb_text_load (db, ".text_load_simple");
+	unlink (".text_load_simple");
+
+	mu_assert_true (succ, "load success");
+	Sdb *ref_db = text_ref_simple_db ();
+	bool eq = sdb_diff (ref_db, db, diff_cb, NULL);
+	sdb_free (ref_db);
+	sdb_free (db);
+	mu_assert_true (eq, "load correct");
 	mu_end;
 }
 
@@ -219,6 +492,14 @@ int all_tests() {
 	mu_run_test (test_sdb_list_big);
 	mu_run_test (test_sdb_foreach_filter);
 	mu_run_test (test_sdb_copy);
+	mu_run_test (test_sdb_text_save_simple);
+	mu_run_test (test_sdb_text_save_simple_unsorted);
+	mu_run_test (test_sdb_text_load_simple);
+	mu_run_test (test_sdb_text_save);
+	mu_run_test (test_sdb_text_load);
+	mu_run_test (test_sdb_text_load_bad_nl);
+	mu_run_test (test_sdb_text_load_broken);
+	mu_run_test (test_sdb_text_load_file);
 	return tests_passed != tests_run;
 }
 
