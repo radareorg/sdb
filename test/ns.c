db718d710f0c612de036487bef9cb7ce64c88bbc
pancake <pancake@nopcode.org>
2020-12-25 05:11:24 +0100

    Add test for sdb_ns and sdb_ns_

--- test/ns.c
+++ test/ns.c
@@ -6,16 +6,24 @@ int main() {
 	Sdb *a = sdb_new0 ();
 	sdb_set (a, "hello", "world", 0);
 	sdb_ns_set (db, "foo", a);
+	Sdb *b = sdb_ns (db, "bar", 1);
+	sdb_set (b, "hello", "world", 0);
 	SdbNs *ns;
 	SdbListIter *it;
+	int oks = 0;
 	ls_foreach (db->ns, it, ns) {
 		eprintf ("%s\n", ns->name);
 		char *o = sdb_querysf (ns->sdb, NULL, 0, "*");
-		eprintf ("%s\n", o);
-		if (!strcmp (o, "hello=world\n")) {
-			eprintf ("Works\n");
-			rc = 0;
+		if (o) {
+			eprintf ("%s\n", o);
+			if (!strcmp (o, "hello=world\n")) {
+				eprintf ("Works\n");
+				oks++;
+			}
 		}
+	}
+	if (oks == 2) {
+		rc = 0;
 	}
 	sdb_free (db);
 	return rc;
