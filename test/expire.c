fb36368a0660bf7535f4de860ebe8597b4d91d00
pancake <pancake@nopcode.org>
2020-12-27 11:15:31 +0100

    Rip windows fixes from Rizin and improve them a bit

--- test/expire.c
+++ test/expire.c
@@ -1,14 +1,23 @@
 #include <sdb.h>
+#if defined _WIN32
+#include <synchapi.h>
+#define sleep(x) Sleep(x*1000)
+#else
 #include <assert.h>
+#endif
 
 int main() {
 	Sdb *s = sdb_new0 ();
 	sdb_set (s, "key", "val", 0);
 	sdb_expire_set (s, "key", 3, 0);
 	sleep (1);
-	assert (sdb_const_get (s, "key", 0) != NULL);
-	sleep (3);
-	assert (sdb_const_get (s, "key", 0) == NULL);
+	if (sdb_const_get (s, "key", 0) == NULL) {
+		return 1;
+	}
+	sleep (4);
+	if (sdb_const_get (s, "key", 0) != NULL) {
+		return 2;
+	}
 	sdb_free (s);
 	return 0;
 }
