e16b4ddd391eed2f4bf1916131ff1825e46e4ae2
pancake <pancake@nopcode.org>
2022-11-18 11:47:22 +0100

    more movings

--- include/sdb/rangstr.h
+++ include/sdb/rangstr.h
@@ -13,7 +13,7 @@ typedef struct {
 	const char *p;
 } Rangstr;
 
-#if 0
+#if 1
 SDB_IPI void rangstr_print (Rangstr *s);
 SDB_IPI Rangstr rangstr_new (const char *s);
 SDB_IPI Rangstr rangstr_null(void);
--- src/json/path.c
+++ src/json/path.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "rangstr.c"
+#include "sdb/rangstr.h"
+#include "sdb/sdb.h"
 #include "sdb/types.h"
 
 SDB_IPI void json_path_first(Rangstr *s) {
